eca86463172ba1359f39b3da880e87fb165917e2
Adebayo Adeniyan <adebeslick@gmail.com>
2017-02-17 15:13:19 +0100

    First Commit

---
+++ include/Config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+*Database config variables
+*/
+
+define("DB_HOST", "localhost");
+define("DB_USERNAME","root");
+define("DB_PASSWORD","");
+define("DB_DATABASE","test_api");
+
+
+?>
---
+++ include/DB_Connect.php
@@ -0,0 +1,26 @@
+<?php
+
+class DB_Connect{
+
+	private conn;
+
+/*
+*connecting to database
+*/
+public function connect(){
+
+require_once 'include/Config';
+
+this->conn = new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+return this->conn;
+
+
+
+
+}
+
+}
+
+
+?>
